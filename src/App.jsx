fe65db722fe156e04e2854093a5e1639af58fccd
subash.shrestha <subash.shrestha@md.p2h.com>
2025-07-02 21:54:04 +0545

    feat: update clear all button

--- src/App.jsx
+++ src/App.jsx
@@ -51,6 +51,11 @@ function App() {
     localStorage.setItem("todolist", JSON.stringify(updatedTodos));
   };
 
+  const handleClearAll = () => {
+    setTodos([]);
+    localStorage.setItem("todolist", JSON.stringify([]));
+  };
+
   return (
     <>
       <div className="flex">
@@ -160,6 +165,16 @@ function App() {
                   </div>
                 );
               })}
+
+              {filteredTodos.length > 0 && (
+                <button
+                  type="button"
+                  onClick={() => handleClearAll()}
+                  className="focus:outline-none text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex cursor-pointer"
+                >
+                  Clear All
+                </button>
+              )}
             </div>
           </div>
         </div>
